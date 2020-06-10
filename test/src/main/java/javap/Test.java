578e913bcb39489a127deeafbe38be9e1e35f490
matio1314520 <1002438709@qq.com>
2020-06-10 12:28:24 +0800

    springboot214++++

---
+++ test/src/main/java/javap/Test.java
@@ -0,0 +1,17 @@
+package javap;
+
+/**
+ * @author mawt
+ * @description
+ * @date 2020/6/10
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = b-a;
+        System.out.println(c);
+    }
+
+}
